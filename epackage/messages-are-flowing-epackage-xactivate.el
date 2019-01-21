c8277e5fd8b83f789b5bbd49b675c0ed25238c89
Jari Aalto <jari.aalto@cante.net>
2019-01-21 18:37:54 +0200

    messages-are-flowing-epackage-xactivate.el: (provide): correct

--- epackage/messages-are-flowing-epackage-xactivate.el
+++ epackage/messages-are-flowing-epackage-xactivate.el
@@ -1,3 +1,3 @@
 (autoload 'messages-are-flowing-use-and-mark-hard-newlines "messages-are-flowing" "" t)
 (add-hook 'message-mode-hook 'messages-are-flowing-use-and-mark-hard-newlines)
-(provide 'messages-are-flowing-epackage-install)
+(provide 'messages-are-flowing-epackage-xactivate)
